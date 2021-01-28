7a57d9078e625776bcdf7c169ccbf6fce80e1c50
ashortsleeves <ashortsleeves@gmail.com>
2021-01-28 16:27:40 -0500

    initial setup of design and colors

--- wp-content/themes/srwc-theme/resources/assets/scripts/routes/common.js
+++ wp-content/themes/srwc-theme/resources/assets/scripts/routes/common.js
@@ -4,5 +4,26 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+
+    var removeClass = true;
+    $('.hamburger').click(function () {
+      $('.hamburger').toggleClass('is-active');
+      $('#sideNav').toggleClass('sideNav-open');
+      $('.sideNavBody').toggleClass('sideNavBody-push');
+      removeClass = false;
+    });
+
+    $('.sideNav').click(function() {
+      removeClass = false;
+    });
+
+    document.addEventListener('touchstart', function(e) {
+      if (removeClass && !$(e.target).hasClass('sideNav') && $('.sideNav').has($(e.target)).length === 0) {
+         $('.hamburger').removeClass('is-active');
+         $('#sideNav').removeClass('sideNav-open');
+         $('.sideNavBody').removeClass('sideNavBody-push');
+      }
+      removeClass = true;
+    }, false);
   },
 };
