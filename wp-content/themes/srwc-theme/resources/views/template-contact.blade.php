ab6be1de8e780ba5257883bfbf710f399aab9939
ashortsleeves <ashortsleeves@gmail.com>
2021-02-16 10:11:09 -0500

    new logo

--- wp-content/themes/srwc-theme/resources/views/template-contact.blade.php
+++ wp-content/themes/srwc-theme/resources/views/template-contact.blade.php
@@ -15,15 +15,12 @@
         <div class="col-md-6 col-content contact-map">
           <section class="footer-map cardstyle">
           {!!$footer['map-content']!!}
-          <div class="map-responsive">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.645434764985!2d-70.65992268449878!3d43.75945917911777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb2ef84e4dc31cf%3A0xa8d2ccc644652cdf!2sSaco%20River%20Wildlife%20Center!5e0!3m2!1sen!2sus!4v1612806638186!5m2!1sen!2sus" width="600" height="330" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-          </div>
-
-
+            <div class="map-responsive">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2881.645434764985!2d-70.65992268449878!3d43.75945917911777!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cb2ef84e4dc31cf%3A0xa8d2ccc644652cdf!2sSaco%20River%20Wildlife%20Center!5e0!3m2!1sen!2sus!4v1612806638186!5m2!1sen!2sus" width="600" height="330" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
           </section>
         </div>
       </div>
-
     </div>
   @endwhile
 @endsection
